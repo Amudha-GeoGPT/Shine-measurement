219d55b765d506e852f4936ce3ba1c7148c9c122
Neshanth <NTS0158@hepl.local>
2024-12-19 15:52:17 +0530

    updated code

--- src/components/Mainpage/SideMenu/SideMenu.jsx
+++ src/components/Mainpage/SideMenu/SideMenu.jsx
@@ -1,59 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ReactSVG } from "react-svg";
+import { Link } from "react-router-dom";
 import s from "./SideMenu.module.scss";
 
 // Icons
 import BookIcon from "../../../assets/svg/book_4.svg";
-import Back from "../../../assets/svg/backward_arrow.svg"; // Replace with your icon paths
+// Replace with your icon paths
+import Back from "../../../assets/svg/backward_arrow.svg"; 
 
 const SideMenu = () => {
-  // const [isCollapsed, setIsCollapsed] = useState(false);
-  // useEffect(() => {     
-  //   // Function to check the window width
-  //   const checkWindowWidth = () => {      
-
-  //     if(window.innerWidth < 800){
-  //       setIsCollapsed(true);
-  //     }
-  //     else{
-  //       setIsCollapsed(false);
-  //     }
-  //           };     
-     
-  //      checkWindowWidth();     
-  //      window.addEventListener('resize', checkWindowWidth);     
-   
-  //      return () => { window.removeEventListener('resize', checkWindowWidth);
-
-  //       };
-  //     },[])
-  // const toggleSidebar = () => {
-  //   setIsCollapsed(!isCollapsed);
-  // };
-
   return (
-    
-      <nav className={s.nav}>
-        {/* Logo Section */}
-   
-
-        {/* Main Menu */}
-        <div className={s.menu}>
+    <nav className={s.nav}>
+      {/* Main Menu */}
+      <div className={s.menu}>
         <h5 className={s.fs_14}>MAIN MENU</h5>
-          <div className={s.menuSection}>
-            <ReactSVG src={BookIcon} className={s.menuIcon} />
-            <h2 className={s.menuTitle}>Experiments</h2>
-          </div>
+        <div className={s.menuSection}>
+          <ReactSVG src={BookIcon} className={s.menuIcon} />
+          <Link to="/" className={s.menuTitle}>
+            Experiments
+          </Link>
         </div>
-
-        {/* Toggle Button */}
-         {/* <button className={s.sidebarToggle} onClick={toggleSidebar}>
-        <ReactSVG src={Back} className={s.menuIcon} />
-      
-        </button>  */}
-
-      </nav>
-  
+      </div>
+    </nav>
   );
 };
 
