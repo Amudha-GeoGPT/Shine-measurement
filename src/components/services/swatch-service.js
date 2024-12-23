89018dfa9a1841c1b2b143b83a5589449643df87
Gowsalya <gowsalya@hepl.com>
2024-12-23 14:54:32 +0530

    Source integration

--- src/components/services/swatch-service.js
+++
@@ -1,7 +0,0 @@
-
-import {  SWATCHNAME, } from "../constants/api-constants";
-import { apiGet, apiPost } from "../../utils/axios";
-
-export const newSwatchName =()=>{
-    return  apiGet(`${SWATCHNAME}`);
-}
--- src/components/services/swatchListView-Service.js
+++
@@ -1,6 +0,0 @@
-import {  SWATCHLIST } from "../constants/api-constants";
-import { apiGet, apiPost } from "../../utils/axios";
-
-export const getBySwatchlist =()=>{
-    return  apiGet(`${SWATCHLIST}`);
-}
