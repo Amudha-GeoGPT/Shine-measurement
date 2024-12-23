ba5d535023bc73023607bae9f498f091fb8d65fc
Surendhar <A092404904@hepl.local>
2024-12-23 12:37:06 +0530

    updated source

--- src/components/services/calculation-service.js
+++ src/components/services/calculation-service.js
@@ -1,10 +1,4 @@
 
-// import { CALCULATION, } from "../constants/api-constants";
-// import { apiGet, apiPost } from "../../utils/axios";
-
-// export const getByCalculate =()=>{
-//     return  apiPost(`${CALCULATION}`);
-// }
 
 import { CALCULATION } from "../constants/api-constants";
 import { apiPost } from "../../utils/axios";
