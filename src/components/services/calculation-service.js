8f5bf3494bfa1f255814a99b42b4baeacc212453
Surendhar <A092404904@hepl.local>
2024-12-23 15:37:27 +0530

    src integration

--- src/components/services/calculation-service.js
+++
@@ -1,8 +0,0 @@
-
-
-import { CALCULATION } from "../constants/api-constants";
-import { apiPost } from "../../utils/axios";
-
-export const getByCalculate = (swatchName) => {
-  return apiPost(`${CALCULATION}`, { swatch_name: swatchName });
-};
--- src/components/services/swatch-service.js
+++
@@ -1,7 +0,0 @@
-
-import {  SWATCHNAME, } from "../constants/api-constants";
-import { apiGet } from "../../utils/axios";
-
-export const newSwatchName =()=>{
-    return  apiGet(`${SWATCHNAME}`);
-}
